cfd9f8b321e368085158114a9a527344508fc305
WhiteMasky <zhangyichichn@outlook.com>
2024-09-16 21:45:47 +1000

    export

--- src/index.js
+++ src/index.js
@@ -4,6 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+export { default as CharacterStatus } from './game-components/CharacterStatus';
+export { default as DialogueBox } from './game-components/DialogueBox';
+export { default as Inventory } from './game-components/Inventory';
+export { default as MiniMap } from './game-components/MiniMap';
+export { default as Navigation } from './game-components/Navigation';
+export { default as QuestLog } from './game-components/QuestLog';
+
+export { default as EmotionalStateMonitor } from './monitor-components/EmotionalStateMonitor';
+export { default as HapticFeedbackSimulator } from './monitor-components/HapticFeedbackSimulator';
+export { default as HealthMonitor } from './monitor-components/HealthMonitor';
+export { default as NeuralInputMonitor } from './monitor-components/NeuralInputMonitor';
+export { default as SettingsPanel } from './monitor-components/SettingsPanel';
+export { default as VRARInterface } from './monitor-components/VRARInterface';
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
